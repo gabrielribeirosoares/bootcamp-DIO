2e2faa57d93b1f76733b5d5dc50f8acbe0fcf36e
Gabriel Ribeiro <gabrielribeirosoares@hotmail.com>
2023-08-23 15:26:46 -0300

    create ide-java

---
+++ ide-java/java-anatomia-classes/src/edu/gabriel/segundasemana/MinhaClasse.java
@@ -0,0 +1,21 @@
+package edu.gabriel.segundasemana;
+
+
+
+public class MinhaClasse {
+
+    public static void main(String[] args) {
+        String primeiroNome = "Gabriel";
+        String segundoNome = " Ribeiro";
+
+        String nomeCompleto = nomeCompleto(primeiroNome,segundoNome);
+        System.out.println(nomeCompleto);
+
+    }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return "Resultado do método: " + primeiroNome.concat("").concat(segundoNome);
+
+    }
+
+}
