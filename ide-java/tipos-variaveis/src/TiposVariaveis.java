e5dccf73e2e01f45c20d7a587f1ab69a786fe49c
Gabriel Ribeiro <gabrielribeirosoares@hotmail.com>
2023-08-29 15:36:20 -0300

    create new doc

--- ide-java/tipos-variaveis/src/TiposVariaveis.java
+++ ide-java/tipos-variaveis/src/TiposVariaveis.java
@@ -1,3 +1,10 @@
 public class TiposVariaveis {
-    
+    public static void main(String[] args) {
+        double SalarioMinimo = 2500; // para milhar, escrever o número inteiro
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        final short numeroCurto2 = (short) numeroNormal;
+
+    }
 }
