a3d4cc86788d1c29d8a0a6d05797b2abb0e60ede
Gabriel Ribeiro <gabrielribeirosoares@hotmail.com>
2023-09-22 16:38:31 -0300

    att

--- ide-java/exemplo-pacotes1/src/escola/Escola.java
+++ ide-java/exemplo-pacotes1/src/escola/Escola.java
@@ -1,11 +1,11 @@
 package escola;
 
 public class Escola {
-        public static void main(String[]args){
-            Aluno felipe = new Aluno();
+	public static void main(String[] args) {
+		Aluno felipe = new Aluno();
+		felipe.setNome("Felipe");
+		felipe.setIdade(8);
 
-            felipe.nome = "Felipe";
-            felipe.idade = 8;
-            System.out.println("O aluno " + felipe.nome + " tem " + felipe.idade + " anos.");
-        }
+		System.out.println("O aluno " + felipe.getNome() + " tem " + felipe.getIdade() + " anos ");
+	}
 }
---
+++ ide-java/exemplo-pacotes2/src/Carro.java
@@ -0,0 +1,11 @@
+public class Carro {
+    public void ligar(){
+        System.out.println("CARRO LIGADO");
+    }
+    private void confereCombustivel(){
+        System.out.println("CONFERINDO COMBUSTÍVEL");
+    }
+    private void confereCambio(){
+        System.out.println("CONFERINDO CÂMBIO EM P");
+    }
+}
