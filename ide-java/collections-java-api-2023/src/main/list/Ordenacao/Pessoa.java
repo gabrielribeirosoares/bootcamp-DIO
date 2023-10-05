04c7fcd27c32db5097a808a2769cfd2d9114ab0d
Gabriel Ribeiro <gabrielribeirosoares@hotmail.com>
2023-10-05 11:14:40 -0300

    att

---
+++ ide-java/collections-java-api-2023/src/main/list/Ordenacao/Pessoa.java
@@ -0,0 +1,58 @@
+package main.list.Ordenacao;
+
+import java.util.Comparator;
+
+public class Pessoa implements Comparable<Pessoa>{
+    private String nome;
+    private int idade;
+    private double altura;
+
+    public Pessoa(String nome, int idade, double altura) {
+        this.nome = nome;
+        this.idade = idade;
+        this.altura = altura;
+    }
+
+    @Override
+    public int compareTo(Pessoa p) {
+        return Integer.compare(idade, p.getIdade());
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa [nome=" + nome + ", idade=" + idade + ", altura=" + altura + "]";
+    }
+
+    }
+
+    class ComparetorPorAltura implements Comparator<Pessoa> {
+
+        @Override
+        public int compare(Pessoa p1, Pessoa p2) {
+            return Double.compare(p1.getAltura(), p2.getAltura());
+        }
+
+    public static void main(String[] args) {
+        OrdenacaoPessoa ordenacaoPessoa = new OrdenacaoPessoa();
+        ordenacaoPessoa.adicionarPessoa("Nome 4", 20, 1.56);
+        ordenacaoPessoa.adicionarPessoa("Nome 2", 30, 1.80);
+        ordenacaoPessoa.adicionarPessoa("Nome 3", 25, 1.70);
+        ordenacaoPessoa.adicionarPessoa("Nome 1", 17, 1.56);
+
+        System.out.println(ordenacaoPessoa.ordenarPorAltura());
+        //System.out.println(ordenacaoPessoa.ordenarPorIdade());
+
+    }
+}
