524e6a6c32f018044bad7cdd77a0fbec67f91778
Gabriel Ribeiro <gabrielribeirosoares@hotmail.com>
2023-10-11 13:42:13 -0300

    att

--- ide-java/Stream-API/src/Desafios/desafio-iphone/src/AparelhoTelefonico.java
+++
@@ -1,5 +0,0 @@
-public interface AparelhoTelefonico {
-    public void ligar(String numero);
-    public void atender(String numero);
-    public void iniciarCorreioVoz(String mensagem, String destinatario);
-}
--- ide-java/Stream-API/src/Desafios/desafio-iphone/src/NavegadorInternet.java
+++
@@ -1,6 +0,0 @@
-
-    public interface NavegadorInternet {
-    public void exibirPagina(String url);
-    public void adicionarNovaAba(String url);
-    public void atualizarPagina();
-}
